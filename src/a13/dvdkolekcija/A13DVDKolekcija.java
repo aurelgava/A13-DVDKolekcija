e17692f42fd7279247ab412c8db51425d73efd15
Aurel Gava <aurel.gava@etsntesla.edu.rs>
2023-05-23 21:08:05 +0200

    Prvi prozor

---
+++ src/a13/dvdkolekcija/A13DVDKolekcija.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a13.dvdkolekcija;
+
+/**
+ *
+ * @author aurel
+ */
+public class A13DVDKolekcija {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new GlavniProzor().setVisible(true);
+    }
+    
+}
